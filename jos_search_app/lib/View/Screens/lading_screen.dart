8c1cb88dff4d1c99d3741951de9f7ff75836553a
Frankelly Guzman <frankellydavidguzman@gmail.com>
2021-10-07 21:25:04 -0400

    new_design_added_unstable

---
+++ jos_search_app/lib/View/Screens/lading_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LadingScreen extends StatelessWidget {
+  const LadingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+a
