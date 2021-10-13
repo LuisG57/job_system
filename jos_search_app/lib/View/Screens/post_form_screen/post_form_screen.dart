0d5dd9738f518ee2303e7d834364c41aa902682c
LuisG57 <luisguzman271001@gmail.com>
2021-10-13 01:10:26 -0400

    Update post_form_screen.dart

--- jos_search_app/lib/View/Screens/post_form_screen/post_form_screen.dart
+++ jos_search_app/lib/View/Screens/post_form_screen/post_form_screen.dart
@@ -47,6 +47,98 @@ class PostFormScreen extends StatelessWidget {
                 height: 750,
                 width: 950,
                 color: Colors.white,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 35.0),
+                      child: Text(
+                        'Publicar oferta de trabajo',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w800,
+                            height: 0.9,
+                            fontSize: 48),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      // crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 300,
+                          child: TextFormField(
+                            decoration: InputDecoration(hintText: 'Compañia'),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Container(
+                          width: 300,
+                          child: TextFormField(
+                            decoration: InputDecoration(hintText: 'Posición'),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      // crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 450,
+                          child: TextFormField(
+                            decoration: InputDecoration(hintText: 'Ubicación'),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Buscar en el mapa'),
+                          style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.green)),
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Container(
+                        width: 600,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              hintText: 'Descripcion...',
+                              border: OutlineInputBorder()),
+                          maxLines: 4,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Publicar'),
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Cancelar'),
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.red),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ],
           )
