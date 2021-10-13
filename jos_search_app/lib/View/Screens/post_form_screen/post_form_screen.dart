fe47d62bb3bee21c5b397931de2b824663863ad5
LuisG57 <luisguzman271001@gmail.com>
2021-10-12 23:16:11 -0400

    added_post_form_scaffold_and_logo

--- jos_search_app/lib/View/Screens/post_form_screen/post_form_screen.dart
+++ jos_search_app/lib/View/Screens/post_form_screen/post_form_screen.dart
@@ -12,8 +12,43 @@ class PostFormScreen extends StatelessWidget {
         alignment: Alignment.center,
         children: [
           GradientBackGround(),
+          //Extract widget later ----*----*----
           Center(
-            child: Container(),
+            child: Container(
+              height: 675,
+              width: 1400,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  colors: [Colors.lightBlueAccent, Colors.greenAccent],
+                ),
+              ),
+              child: Container(
+                padding: EdgeInsets.only(left: 50, right: 50, bottom: 50),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Image.asset(
+                    'images/post_form_icon.png',
+                    height: 400,
+                  ),
+                ),
+                alignment: Alignment.centerLeft,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 350,
+              ),
+              Container(
+                height: 750,
+                width: 950,
+                color: Colors.white,
+              ),
+            ],
           )
         ],
       ),
