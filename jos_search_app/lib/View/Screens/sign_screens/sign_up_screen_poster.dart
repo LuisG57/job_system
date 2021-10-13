a4043c22ff43c3030a2004fd79bcc3d8d948eff1
LuisG57 <luisguzman271001@gmail.com>
2021-10-12 20:46:26 -0400

    Extracted widgets from sign up poster

--- jos_search_app/lib/View/Screens/sign_screens/sign_up_screen_poster.dart
+++ jos_search_app/lib/View/Screens/sign_screens/sign_up_screen_poster.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:jos_search_app/View/Widgets/form_widgets/sign_up_button.dart';
+import 'package:jos_search_app/View/Widgets/form_widgets/sign_up_text_form_field.dart';
 import 'package:jos_search_app/View/Widgets/gradient_background/gradient_background.dart';
 import 'package:jos_search_app/View/Widgets/gradient_background/gradient_background_white_board.dart';
 
@@ -41,101 +43,104 @@ class SignUpScreenPoster extends StatelessWidget {
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(30)),
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Text(
-                        'Bienvenido(a),\nAyudanos a conocerte mejor.',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w800,
-                            height: 0.9,
-                            fontSize: 48),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 35.0),
+                        child: Text(
+                          'Bienvenido(a),\nAyudanos a conocerte mejor.',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w800,
+                              height: 0.9,
+                              fontSize: 48),
+                        ),
                       ),
-                      Form(
-                        child: Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Container(
-                                  width: 300,
-                                  child: TextFormField(
-                                    decoration:
-                                        InputDecoration(hintText: 'Nombre'),
+                      Expanded(
+                        child: Form(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SignUpTextFormField(
+                                      hintText: 'Nombre', Width: 300),
+                                  SizedBox(
+                                    width: 25,
                                   ),
-                                ),
-                                SizedBox(
-                                  width: 25,
-                                ),
-                                Container(
-                                  width: 300,
-                                  child: TextFormField(
-                                    decoration:
-                                        InputDecoration(hintText: 'Apellido'),
+                                  SignUpTextFormField(
+                                      hintText: 'Apellido', Width: 300),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SignUpTextFormField(
+                                      hintText: 'Correo Electronico',
+                                      Width: 350),
+                                  SignUpTextFormField(
+                                      hintText: 'Contraseña', Width: 250),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SignUpTextFormField(
+                                      hintText: 'Numero de Teléfono',
+                                      Width: 350),
+                                  SizedBox(
+                                    width: 25,
                                   ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Container(
-                                  width: 350,
+                                  Container(
+                                    width: 250,
+                                    child: DropdownButtonFormField(
+                                      items: [
+                                        DropdownMenuItem(
+                                          child: Text('Categoria'),
+                                        ) //we need to figure out how to include more than one dropdown item
+                                      ],
+                                      decoration: InputDecoration(
+                                          hintText: 'Categoria'),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 8.0),
+                                child: Container(
+                                  width: 600,
                                   child: TextFormField(
                                     decoration: InputDecoration(
-                                        hintText: 'Correo Electronico'),
+                                        hintText: 'Descripcion...',
+                                        border: OutlineInputBorder()),
+                                    maxLines: 4,
                                   ),
-                                ),
-                                SizedBox(
-                                  width: 25,
-                                ),
-                                Container(
-                                  width: 250,
-                                  child: TextFormField(
-                                    decoration:
-                                        InputDecoration(hintText: 'Contraseña'),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Container(
-                                  width: 350,
-                                  child: TextFormField(
-                                    decoration: InputDecoration(
-                                        hintText: 'Numero de Teléfono'),
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: 25,
-                                ),
-                                Container(
-                                  width: 250,
-                                  child: DropdownButtonFormField(
-                                    items: [
-                                      DropdownMenuItem(
-                                          child: Text(
-                                              'Categoria')) //we need to figure out how to include more than one dropdown item
-                                    ],
-                                    decoration:
-                                        InputDecoration(hintText: 'Categoria'),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(vertical: 8.0),
-                              child: Container(
-                                width: 600,
-                                child: TextFormField(
-                                  decoration: InputDecoration(
-                                      hintText: 'Descripcion...',
-                                      border: OutlineInputBorder()),
-                                  maxLines: 4,
                                 ),
                               ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SignUpButton(buttonText: 'Crear Cuenta', function: () {}),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Ya tienes una cuenta?',
+                              style: TextStyle(fontSize: 18),
                             ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            TextButton(
+                                onPressed: () {},
+                                child: Text(
+                                  'Iniciar Sesion',
+                                  style: TextStyle(fontSize: 18),
+                                ))
                           ],
                         ),
                       )
